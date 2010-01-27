71260762e97d11ed4231c16362e3666fc0aefcf0
miaka <miaka@Macintosh.local>
2010-01-27 21:35:08 +0100

    pages_set fix

--- src/home/views.py
+++ src/home/views.py
@@ -11,7 +11,8 @@ def index(request):
     promotions  = get_object(Page, 'code =','home-promotions')
     
     news = get_object(Category, 'code =', 'news')
-    current_news = news.pages_set.order('date_added').fetch(1)
-    current_news = current_news[0] if current_news else None
+    if news:
+        current_news = news.pages_set.order('date_added').fetch(1)
+        current_news = current_news[0] if current_news else None
     
     return direct_to_template(request, 'home.html', locals())
