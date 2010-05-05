a3d83d07a78297d50b3f6d6cdb33a78fa524ba88
Michał Kłujszo <michal.klujszo@10clouds.com>
2010-05-05 23:20:34 +0200

    some changes from miaka

--- src/runserver.sh
+++ src/runserver.sh
@@ -1,1 +1,1 @@
-/System/Library/Frameworks/Python.framework/Versions/2.6/bin/python2.6 manage.py runserver
+/System/Library/Frameworks/Python.framework/Versions/2.6/bin/python2.6 manage.py runserver --datastore_path ./.datastore
